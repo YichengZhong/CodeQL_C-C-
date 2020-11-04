518e091edb69894fb416c77cee84335504541ef6
YichengZhong <1165240284@qq.com>
2020-11-04 22:17:28 +0800

    Protobuf检查

--- 关键脚本.sh
+++ 关键脚本.sh
@@ -8,25 +8,15 @@ codeql database create ./struts_db -s ./struts_9805 /
   
 /home/zyc/codeql-linux64/codeql/codeql database create ./database_test --language=cpp --source-root='./Test_Cl' --command=make 
 /home/zyc/codeql-linux64/codeql/codeql database create ./database_test --language=cpp --source-root='./protobuf' --command=make 
-
 /home/zyc/codeql-linux64/codeql/codeql database create ./redis_test --language=cpp --source-root='./redis-6.0.9' --command=make 
-
 ./codeql database create ./database_test -l cpp -s ./Test_Cl --command "make" 
-
 /home/zyc/codeql-linux64/codeql/codeql database analyze ./database_test emptyblock.ql --format=csv --output=first.csv
-
 codeql-main/codeql-main/cpp/ql/src/codeql-suites
-
 /home/zyc/codeql-linux64/codeql/codeql database analyze ./database_test ./codeql-linux64/codeql-main/cpp/ql/src/codeql-suites/cpp-lgtm.qls --format=csv --output=first.csv
-
 /home/zyc/codeql-linux64/codeql/codeql database upgrade ./database_test
-
 /home/zyc/codeql-linux64/codeql/codeql database upgrade ./redis_test
-
 /home/zyc/codeql-linux64/codeql/codeql database upgrade ./redis_test
-
 /home/zyc/codeql-linux64/codeql/codeql database analyze ./redis_test ./codeql-linux64/codeql-main/cpp/ql/src/codeql-suites/cpp-lgtm.qls --format=csv --output=redis_test.csv
-
 /home/zyc/codeql-linux64/codeql/codeql database analyze ./protobuf_database ./codeql-linux64/codeql-main/cpp/ql/src/codeql-suites/cpp-lgtm.qls --format=csv --output=protobuf_check.csv
 
 踩坑的小经验：
